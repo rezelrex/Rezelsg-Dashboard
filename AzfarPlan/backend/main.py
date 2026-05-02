b097054a197d748e28443f4a42b1f2c326ffe908
Azfar Yusri <azfar200@gmail.com>
2026-05-03 04:47:14 +0800

    Fix CORS preflight by disabling credentials

--- AzfarPlan/backend/main.py
+++ AzfarPlan/backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
     allow_origins=[
         "https://rezelsg-dashboard.vercel.app/"
         ], 
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
